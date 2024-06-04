c9f244fbeb87021b5a3d582314a054923f4f0bdc
梁容 <1398193136@qq.com>
2024-06-04 16:43:05 +0800

    "单例封装时间和日志工具类"

--- app/src/main/java/com/example/meet/MainActivity.java
+++ app/src/main/java/com/example/meet/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.meet;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.framework.utils.LogUtils;
+import com.example.framework.utils.TimeUtil;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,5 +35,13 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+            LogUtils.i(TimeUtil.formatDuring(System.currentTimeMillis()));
+
+
+
     }
+
+
+
+
 }
