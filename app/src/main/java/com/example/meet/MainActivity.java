c38efe6097d35e8a5ce9a528748091d4e1c6f15b
梁容 <1398193136@qq.com>
2024-06-06 21:45:22 +0800

    实现引导页

--- app/src/main/java/com/example/meet/MainActivity.java
+++ app/src/main/java/com/example/meet/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.meet;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -19,6 +22,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.framework.base.BaseUIActivity;
+import com.example.framework.manager.MediaPlayerManager;
 import com.example.framework.utils.LogUtils;
 import com.example.framework.utils.TimeUtil;
 
@@ -26,7 +31,7 @@ import java.io.File;
 import java.io.IOException;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseUIActivity {
 
 
     @Override
@@ -35,13 +40,18 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-            LogUtils.i(TimeUtil.formatDuring(System.currentTimeMillis()));
+        MediaPlayerManager mediaPlayerManager = new MediaPlayerManager();
+        AssetFileDescriptor fileDescriptor = getResources().openRawResourceFd(R.raw.guide);
+        mediaPlayerManager.startPlay(fileDescriptor);
 
-
+        mediaPlayerManager.setOnProgressListener(new MediaPlayerManager.OnMusicProgressListener() {
+            @Override
+            public void OnProgress(int progress,int pos) {
+               // LogUtils.e("progress:" + progress + "All:" + mediaPlayerManager.getDuration());//progress:30245All:51144
+               int p = (int)(((float)progress) / ((float)mediaPlayerManager.getDuration()) * 100);
+               LogUtils.e("p:" + p);
+            }
+        });
 
     }
-
-
-
-
 }
